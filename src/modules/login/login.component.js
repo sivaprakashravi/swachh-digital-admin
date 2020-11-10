8fa23626c00cce693281412df1956954f3b17e44
Baskaran Sivaprakasharavi (RBEI/BSL5) <sba5cob@bosch.com>
2020-11-10 18:32:18 +0530

    otp login updated

--- src/modules/login/login.component.js
+++ src/modules/login/login.component.js
@@ -8,26 +8,50 @@ class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            isOTPControl: false,
+            rememberMe: false
         };
+    }
+
+    userNameControls() {
+        return (
+            <ul>
+                <li>
+                    <label>Username</label>
+                    <input type="text" />
+                </li>
+                <li>
+                    <label>Password</label>
+                    <input type="password" />
+                </li>
+                <li><span onClick={() => {
+                    this.setState({rememberMe: !this.state.rememberMe})
+                }}><BsCheckBox size="18" color={this.state.rememberMe ? '#3f51b5': null} /><label>Remember Me</label></span><a className="fp" href="http://#">Forgot password?</a></li>
+                <li><button className="primary">Login</button></li>
+                <li className="sign-up">Don't have an account? <a href="http://#">Sign up</a></li>
+            </ul>
+        )
+    }
+
+    otpControls() {
+        return (
+            <ul>
+                <li>
+                    <label>Mobile Number</label>
+                    <input type="text" />
+                </li>
+                <li><button className="primary">Request OTP</button></li>
+                <li className="sign-up">Don't have an account? <a href="http://#">Sign up</a></li>
+            </ul>
+        )
     }
 
     controls() {
         return (
             <div className="controls">
-                <ul>
-                    <li>
-                        <label>Username</label>
-                        <input type="text" />
-                    </li>
-                    <li>
-                        <label>Password</label>
-                        <input type="password" />
-                    </li>
-                    <li><span><BsCheckBox size="18" /><label>Remember Me</label></span><a className="fp" href="http://#">Forgot password?</a></li>
-                    <li><button className="primary">Login</button></li>
-                    <li className="sign-up">Don't have an account? <a href="http://#">Sign up</a></li>
-                </ul>
+                {this.state.isOTPControl ? this.otpControls() : this.userNameControls()}
                 {this.externalControls()}
+                {this.switchControls()}
             </div>)
     }
 
@@ -36,6 +60,16 @@ class Login extends React.Component {
             <div className="external-auth">
                 <button><FaFacebook size="18" color="#3b5998" /></button>
                 <button><FcGoogle size="18" /></button>
+            </div>
+        )
+    }
+
+    switchControls() {
+        return (
+            <div className="otp">
+                <button onClick={() => {
+                    this.setState({isOTPControl: !this.state.isOTPControl})
+                }}>Login with {this.state.isOTPControl ? 'Username' : 'OTP'}</button>
             </div>
         )
     }
