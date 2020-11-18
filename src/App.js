f2dcce921fa9fcc10c49c41b3af2b539cd0a3bd2
Baskaran Sivaprakasharavi (RBEI/BSL5) <sba5cob@bosch.com>
2020-11-18 22:18:18 +0530

    bottom navigation included

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from './modules/register/register.component';
 import Login from './modules/login/login.component';
 import Dashboard from './modules/dashboard/dashboard.component';
 import Header from './shared/header/header.component';
+import BottomNav from './shared/bottom-navigation/bottom-navigation.component';
 
 function showHeader(route) {
   const noAuth = ['', 'login', 'register'];
@@ -17,13 +18,14 @@ function showHeader(route) {
 function App(e) {
   return (
     <main>
-      {showHeader(e.location.pathname) ? <Header></Header>: null}
+      {showHeader(e.location.pathname) ? <Header />: null}
       <Switch>
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/" component={Login} exact />
       </Switch>
+      {showHeader(e.location.pathname) ? <BottomNav />: null}
     </main>
   )
 }
