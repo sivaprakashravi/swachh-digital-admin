7b364f168815afd3fd3df88d575224893141c67c
shanusai <shanmukhasky@gmail.com>
2020-12-07 18:01:45 +0530

    merged orders and auth flow in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Login from './modules/login/login.component';
-<<<<<<< HEAD
 import Register from './modules/register/register.component';
 import Dashboard from './modules/dashboard/dashboard.component'
 import AddProductScreen from './modules/product/addProduct.component'
@@ -32,33 +31,6 @@ switch(action.type){
       isLoading: false,       
     }
   case 'SING_IN_SUCCESS':
-=======
-import Dashboard from './modules/dashboard/dashboard.component'
-import AddProductScreen from './modules/product/addProduct.component'
-import Header from './shared/header/header.component';
-import BottomNav from './shared/bottom-navigation/bottom-navigation.component';
-import { ProductListScreen } from './modules/product/productList.component'
-import { EditScreen } from './modules/product/editProduct';
-import StoreRegister from './modules/register/storeRegister.component'
-import CreateProduct from './modules/baseProduct/createProduct.component'
-import { AuthContext } from './modules/utils/auth-context'
-import fetchApi from './services/fetchsvc';
-
-
-function showHeader(route) {
-  const noAuth = ['', 'login', 'register'];
-  const has = noAuth.filter(auth => {
-    return route === `/${auth}`;
-  });
-  return (has && has.length) ? false : true;
-}
-function App(e) {
-  const [state, dispatch] = React.useReducer(
-    (prevState, action) => {
-      console.log(action.store)
-      switch (action.type) {
-        case 'SING_IN_SUCCESS':
->>>>>>> 08c2d05504ba9b232be4e02c84e111daaa4a6324
           return {
             ...prevState,
             isLoading: false,
@@ -66,7 +38,6 @@ function App(e) {
             ids: action.dataApi,
             user: action.store,
           }
-<<<<<<< HEAD
           case 'REFRESH_TOKEN':
             return{
               ...prevState,
@@ -94,19 +65,6 @@ function App(e) {
     error: null,
   },
 );
-=======
-      }
-    },
-    {
-      isLoading: false,
-      isSignOut: false,
-      userToken: null,
-      user: {},
-      ids: {},
-      error: null,
-    },
-  );
->>>>>>> 08c2d05504ba9b232be4e02c84e111daaa4a6324
 
 const reFresh=async ()=>{
   try {
@@ -128,18 +86,11 @@ React.useEffect(()=>{
       signIn: async (values) => {
 
         try {
-<<<<<<< HEAD
           dispatch({type: 'SING_IN'});
           const data={
               "email" : values.username,
   "password" : values.password,
   "returnSecureToken" : true
-=======
-          const data = {
-            "email": values.username,
-            "password": values.password,
-            "returnSecureToken": true
->>>>>>> 08c2d05504ba9b232be4e02c84e111daaa4a6324
           }
           const dataApi = await fetchApi.logInpost('signInWithPassword', JSON.stringify(data));
           const { idToken, email, localId } = dataApi;
@@ -148,7 +99,6 @@ React.useEffect(()=>{
           {
             "UserId": localId
           }
-<<<<<<< HEAD
          const store = await fetchApi.post('api/getStoreInfo',JSON.stringify(user),idToken);
          await localStorage.setItem('storeUser',JSON.stringify(store));
          
@@ -169,19 +119,6 @@ try {
 } catch (error) {
   console.log(error)
 }
-=======
-          const store = await fetchApi.post('api/getStoreInfo', JSON.stringify(user), idToken);
-          await localStorage.setItem('storeUser', JSON.stringify(store));
-          dispatch({ type: 'SING_IN_SUCCESS', dataApi, store });
-
-        } catch (error) {
-          console.log('login', error)
-        }
-
-      },
-      signOut: async () => {
-
->>>>>>> 08c2d05504ba9b232be4e02c84e111daaa4a6324
       },
       signUp: async () => {
         try {
@@ -189,7 +126,6 @@ try {
         } catch (error) {
 
         }
-<<<<<<< HEAD
       } 
   }),
   []
@@ -236,45 +172,6 @@ console.log( state.userToken)
         }
       
       </Switch>
-=======
-      }
-    }),
-    []
-  )
-
-  return (
-    <main>
-      {/* <Header></Header> */}
-      <AuthContext.Provider value={{ ...authContext, ...state }}>
-        {showHeader(e.location.pathname) ? <Header /> : null}
-        <Switch>
-          {
-            state.userToken == null ?
-              (
-                <>
-                  <Route path="/login" component={Login} />
-                  <Route path="/register" component={Register} />
-                  <Route path="/" component={Login} exact />
-                </>
-              )
-              :
-              (
-                <>
-                  <Route path="/" component={Dashboard} exact />
-                  <Route path="/dashboard" component={Dashboard} />
-                  <Route path="/addproduct" component={AddProductScreen} />
-                  <Route path="/productlist" component={ProductListScreen} />
-                  <Route path="/editScreen" component={EditScreen} />
-                  <Route path="/storeRegister" component={StoreRegister} />
-                  <Route path="/createProduct" component={CreateProduct} />
-                </>
-              )
-          }
-
-        </Switch>
-
-        {showHeader(e.location.pathname) ? <BottomNav /> : null}
->>>>>>> 08c2d05504ba9b232be4e02c84e111daaa4a6324
       </AuthContext.Provider>
     </main>
   )
