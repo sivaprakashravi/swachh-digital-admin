eea256bfddaed8dba7776c9c6ad643c059cc3d55
Baskaran Sivaprakasharavi (RBEI/BSL5) <sba5cob@bosch.com>
2020-12-08 09:29:59 +0530

    conditioning updated

--- src/App.js
+++ src/App.js
@@ -5,32 +5,32 @@ import Register from './modules/register/register.component';
 import Dashboard from './modules/dashboard/dashboard.component'
 import AddProductScreen from './modules/product/addProduct.component'
 // import Header from './shared/header/header.component';
-import {ProductListScreen} from './modules/product/productList.component'
-import {EditScreen} from './modules/product/editProduct'
+import { ProductListScreen } from './modules/product/productList.component'
+import { EditScreen } from './modules/product/editProduct'
 import { Route, Switch } from 'react-router-dom';
-import StoreRegister  from './modules/register/storeRegister.component'
+import StoreRegister from './modules/register/storeRegister.component'
 import CreateProduct from './modules/baseProduct/createProduct.component'
-import {OrderScreen} from './modules/orders/orderScreen.component'
-import {OrderDetails} from './modules/orders/orderDetails.component'
+import { OrderScreen } from './modules/orders/orderScreen.component'
+import { OrderDetails } from './modules/orders/orderDetails.component'
 import { OrderEditScreen } from "./modules/orders/orderEdit.component";
-import {AuthContext} from './modules/utils/auth-context'
+import { AuthContext } from './modules/utils/auth-context'
 import fetchApi from './services/fetchsvc'
 function App() {
-const [state,dispatch] = React.useReducer(
-  (prevState,action)=>{
-    console.log(action.store)
-switch(action.type){
-  case 'SING_IN':
-    return {
-      ...prevState,
-      isLoading: true,       
-    }
-    case 'SING_IN_ERROR':
-    return {
-      ...prevState,
-      isLoading: false,       
-    }
-  case 'SING_IN_SUCCESS':
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      console.log(action.store)
+      switch (action.type) {
+        case 'SING_IN':
+          return {
+            ...prevState,
+            isLoading: true,
+          }
+        case 'SING_IN_ERROR':
+          return {
+            ...prevState,
+            isLoading: false,
+          }
+        case 'SING_IN_SUCCESS':
           return {
             ...prevState,
             isLoading: false,
@@ -38,59 +38,59 @@ switch(action.type){
             ids: action.dataApi,
             user: action.store,
           }
-          case 'REFRESH_TOKEN':
-            return{
-              ...prevState,
-              isLoading: false,
-              userToken: action?.userId?.idToken??null,
-              ids:action?.userId??null,
-              user: action?.store??null, 
-            }
-            case 'SIGN_OUT':
-              return{
-                ...prevState,
-                isLoading: false,
-                userToken: null,
-                ids: null,
-                user: null, 
-              }
-}
-  },
-  {
-    isLoading: false,
-    isSignOut: false,
-    userToken: null,
-    user: {},
-    ids:{},
-    error: null,
-  },
-);
+        case 'REFRESH_TOKEN':
+          return {
+            ...prevState,
+            isLoading: false,
+            userToken: action?.userId?.idToken ?? null,
+            ids: action?.userId ?? null,
+            user: action?.store ?? null,
+          }
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isLoading: false,
+            userToken: null,
+            ids: null,
+            user: null,
+          }
+      }
+    },
+    {
+      isLoading: false,
+      isSignOut: false,
+      userToken: null,
+      user: {},
+      ids: {},
+      error: null,
+    },
+  );
 
-const reFresh=async ()=>{
-  try {
-    const storeParse = await localStorage.getItem('storeUser');
-    const userParse = await localStorage.getItem('userToken');
-    const store = JSON.parse(storeParse);
-    const userId = JSON.parse(userParse);
-    dispatch({type:'REFRESH_TOKEN',userId,store})
-   } catch (error) {
-     console.log(error)
-   }
-}
-React.useEffect(()=>{
- reFresh()
-},[])
+  const reFresh = async () => {
+    try {
+      const storeParse = await localStorage.getItem('storeUser');
+      const userParse = await localStorage.getItem('userToken');
+      const store = JSON.parse(storeParse);
+      const userId = JSON.parse(userParse);
+      dispatch({ type: 'REFRESH_TOKEN', userId, store })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  React.useEffect(() => {
+    reFresh()
+  }, [])
 
   const authContext = React.useMemo(
     () => ({
       signIn: async (values) => {
 
         try {
-          dispatch({type: 'SING_IN'});
-          const data={
-              "email" : values.username,
-  "password" : values.password,
-  "returnSecureToken" : true
+          dispatch({ type: 'SING_IN' });
+          const data = {
+            "email": values.username,
+            "password": values.password,
+            "returnSecureToken": true
           }
           const dataApi = await fetchApi.logInpost('signInWithPassword', JSON.stringify(data));
           const { idToken, email, localId } = dataApi;
@@ -99,26 +99,26 @@ React.useEffect(()=>{
           {
             "UserId": localId
           }
-         const store = await fetchApi.post('api/getStoreInfo',JSON.stringify(user),idToken);
-         await localStorage.setItem('storeUser',JSON.stringify(store));
-         
-         dispatch({type: 'SING_IN_SUCCESS', dataApi, store});
-         
-      } catch (error) {
-        dispatch({type: 'SING_IN_ERROR'});
-          console.log('login',error)
+          const store = await fetchApi.post('api/getStoreInfo', JSON.stringify(user), idToken);
+          await localStorage.setItem('storeUser', JSON.stringify(store));
+
+          dispatch({ type: 'SING_IN_SUCCESS', dataApi, store });
+
+        } catch (error) {
+          dispatch({ type: 'SING_IN_ERROR' });
+          console.log('login', error)
           alert(error)
-      }
+        }
 
       },
-      signOut: async ()=>{
-try {
-  localStorage.removeItem('userToken');
-  localStorage.removeItem('storeUser');
-  dispatch({type:'SIGN_OUT'})
-} catch (error) {
-  console.log(error)
-}
+      signOut: async () => {
+        try {
+          localStorage.removeItem('userToken');
+          localStorage.removeItem('storeUser');
+          dispatch({ type: 'SIGN_OUT' })
+        } catch (error) {
+          console.log(error)
+        }
       },
       signUp: async () => {
         try {
@@ -126,52 +126,35 @@ try {
         } catch (error) {
 
         }
-      } 
-  }),
-  []
-)
+      }
+    }),
+    []
+  )
 
-
-if(state.isLoading){
-    return(
-        <div className="center">
-    <div className="loader"></div>
-    </div>
+  if (state.isLoading) {
+    return (
+      <div className="center">
+        <div className="loader"></div>
+      </div>
     )
-}
-console.log( state.userToken)
+  }
   return (
     <main>
       {/* <Header></Header> */}
-      <AuthContext.Provider value={{...authContext,...state}}>
-      <Switch>
-        {
-          state.userToken === null ?
-          ( 
-            <>
-           <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/" component={Login} exact />
-          </>
-          )
-          :
-          (
-            <>
-             <Route path="/" component={Dashboard} exact />
-             <Route path="/dashboard" component={Dashboard} />
-        <Route path="/addproduct" component={AddProductScreen} />
-        <Route path="/productlist" component={ProductListScreen} />
-        <Route path="/orderDetails" component={OrderDetails} />
-        <Route path="/orderlist" component={OrderScreen} />
-        <Route path="/editScreen" component={EditScreen} />
-        <Route path="/storeRegister" component={StoreRegister} />
-        <Route path="/createProduct" component={CreateProduct} />
-        <Route path="/orderEdit" component={OrderEditScreen} />
-            </>
-          )
-        }
-      
-      </Switch>
+      <AuthContext.Provider value={{ ...authContext, ...state }}>
+        <Switch>
+          <Route path="/" component={state.userToken ? Dashboard : Login} exact />
+          <Route path="/register" component={Register} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/addproduct" component={AddProductScreen} />
+          <Route path="/productlist" component={ProductListScreen} />
+          <Route path="/orderDetails" component={OrderDetails} />
+          <Route path="/orderlist" component={OrderScreen} />
+          <Route path="/editScreen" component={EditScreen} />
+          <Route path="/storeRegister" component={StoreRegister} />
+          <Route path="/createProduct" component={CreateProduct} />
+          <Route path="/orderEdit" component={OrderEditScreen} />
+        </Switch>
       </AuthContext.Provider>
     </main>
   )
