229de21aadf838e4afacab9b4cd78b92e4357f4c
sivaprakasharavi <sivaprakashravi@gmail.com>
2020-12-14 20:14:00 +0530

    order details style updated

--- src/modules/orders/order_details/order_details.component.js
+++ src/modules/orders/order_details/order_details.component.js
@@ -1,46 +1,44 @@
 import React from 'react';
 import '../order_screen.style.scss';
+import { RiArrowGoBackLine } from 'react-icons/ri';
+import { FcCheckmark, FcCancel, FcShop, FcFeedIn, FcShipped, FcCallback, FcButtingIn, FcMoneyTransfer } from 'react-icons/fc';
+import { FaPhoneAlt, FaRupeeSign } from 'react-icons/fa';
+import * as moment from 'moment';
+import { useState } from 'react';
 
 export const OrderDetails = (props) => {
-    const data = props.location.state
+    const [isShowDetails, setShowDetails] = useState(false);
+    const toggle = React.useCallback(() => setShowDetails(!isShowDetails));
+    const data = props.location.state;
     return (
-        <div className="order-details">
-            <div className="row-view">
-                <text style={{ float: 'left', width: '50%', color: 'orange' }}>#{data.id}<br />
-                    <text style={{ fontSize: 12, color: 'gray' }}>{data.date}</text></text>
-                <text style={{ float: 'right', width: '50%', color: 'green' }}>Status : <text>{data.status}</text></text>
+        <div className="order">
+            <div className="sub-header">
+                <RiArrowGoBackLine onClick={props.history.goBack} className="icon" size="22px" />
+                <label>Order Details - {data.id} </label><span>{moment().format('L')}</span>
             </div>
-            <div className="details-view">
-                <ul>
+            <div className="details">
+                <ul className="status">
+                    {/* <li><div><FcCheckmark size="35px" /></div></li>
+                    <li><div><FcShop size="35px" /></div></li>
+                    <li><div><FcFeedIn size="35px" /></div></li> */}
                     <li>
-                        <text>Customer Name : {data.customer_name}</text>
-                    </li>
-                    <li>
-                        <text>Customer Mobile : {data.customer_phone}</text>
-                    </li>
-                    <li>
-                        <text>Email : {data.email}</text>
-                    </li>
-                    <li>
-                        <text>Amount : {data.amount}</text>
-                    </li>
-                    <li>
-                        <text>Link : {data.tracking_link}</text>
-                    </li>
-                    <li>
-                        <text>No.Of Items : {data.items_count}</text>
-                    </li>
-                    <li>
-                        <text>Category : {data.category}</text>
-                    </li>
-                    <li>
-                        <text>Payment Through : {data.paymentType}</text>
-                    </li>
+                        <FcShipped size="100px" />
+                        <label>Shipped</label></li>
                 </ul>
+                <div className="customer" onClick={toggle}>
+                    <div><FcButtingIn size="50px" /></div>
+                    <div>{data.customer_name} <label>{data.customer_phone}</label></div>
+                    <FaPhoneAlt className="call" color="green" size="20px" />
+                    <FcMoneyTransfer className="call" size="20px" />
+                </div>
+                {isShowDetails ?
+                    <ul className="by">
+                        <li>{data.items_count} Item for <FaRupeeSign /> {data.amount}</li>
+                    </ul> : null}
             </div>
-            <div className="button-view">
-                <button className="primary">Accept</button>
-                <button className="primary">Reject</button>
+            <div className="actions">
+                <li><button><FcCheckmark /><label>Accept</label></button></li>
+                <li><button><FcCancel /><label>Reject</label></button></li>
             </div>
         </div>
     )
