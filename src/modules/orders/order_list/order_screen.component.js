e7f367fa28b6179fdfdb2530df41886c35cb6909
sivaprakasharavi <sivaprakashravi@gmail.com>
2020-12-14 20:38:47 +0530

    no orders view updated

--- src/modules/orders/order_list/order_screen.component.js
+++ src/modules/orders/order_list/order_screen.component.js
@@ -15,47 +15,53 @@ export class OrderScreen extends React.Component {
             filter: false,
             recent: true,
             detailed: false,
-            list:[]
+            list: []
         }
     }
 
-    componentDidMount(){
-      this.listFromApi({
-        "IsCurrentDate" : true
-    });
+    componentDidMount() {
+        this.listFromApi({
+            "IsCurrentDate": true
+        });
     }
 
-   async listFromApi(values){
-    const store =  localStorage.getItem('storeUser');
-    const { StoreId } = JSON.parse(store)
-       const data = {
-        "StoreId" : StoreId,
-        ...values
-       }
-      const listdata = await fetchservices.post('api/getOrders',data);
-      this.setState({
-          list:listdata
-      })
+    async listFromApi(values) {
+        const store = localStorage.getItem('storeUser');
+        const { StoreId } = JSON.parse(store)
+        const data = {
+            "StoreId": StoreId,
+            ...values
+        }
+        const listdata = await fetchservices.post('api/getOrders', data);
+        this.setState({
+            list: listdata
+        })
     }
 
-   async handleSelect(select, date){
-      let  startDate = moment(select.range1.startDate).format('DD-MM-YYYY');
-       let endDate = moment(select.range1.endDate).format('DD-MM-YYYY');
-        this.listFromApi({  "FromDate" : startDate,
-        "ToDate" : endDate,
-        "IsCustomDate" : true})
-        await this.setState({filter:false})
-      }
+    async handleSelect(select, date) {
+        let startDate = moment(select.range1.startDate).format('DD-MM-YYYY');
+        let endDate = moment(select.range1.endDate).format('DD-MM-YYYY');
+        this.listFromApi({
+            "FromDate": startDate,
+            "ToDate": endDate,
+            "IsCustomDate": true
+        })
+        await this.setState({ filter: false })
+    }
 
-     async handleChange(event, stateVariable) {
-       await this.setState({ [stateVariable]: event.target.value });
-        this.state.period === 'Today' && this.listFromApi({ "IsCurrentDate" : true})
-        this.state.period === 'This week' && this.listFromApi({ "IsLastWeek" : true})
-       }
+    async handleChange(event, stateVariable) {
+        await this.setState({ [stateVariable]: event.target.value });
+        this.state.period === 'Today' && this.listFromApi({ "IsCurrentDate": true })
+        this.state.period === 'This week' && this.listFromApi({ "IsLastWeek": true })
+    }
+
+    noList() {
+        return (<div className="no-list"><label>No Orders Found</label><button className="primary" onClick={this.props.history.goBack}>Go Back</button></div>)
+    }
     render() {
         const self = this;
-        const {list} = self.state
-        let listMap = list.map((x, index) => {
+        const { list } = self.state;
+        let listMap = list && !list.length ? this.noList() : list.map((x, index) => {
             return (
                 <OrderList data={x} key={index} nav={self.props.history} />
             )
@@ -66,7 +72,10 @@ export class OrderScreen extends React.Component {
         };
         return (
             <div className="orders">
-                <div className="sub-header"><RiArrowGoBackLine onClick={this.props.history.goBack} className="icon" size="22px" /><label>Orders</label> <RiFilterLine className="i-filter" size="22px" onClick={() => this.setState({filter: !this.state.filter})} /></div>
+                <div className="sub-header">
+                    <RiArrowGoBackLine onClick={this.props.history.goBack} className="icon" size="22px" />
+                    <label>Orders</label>
+                    {list && list.length ? <RiFilterLine className="i-filter" size="22px" onClick={() => this.setState({ filter: !this.state.filter })} /> : null}</div>
                 {this.state.filter ? <div className="filter">
                     <ul>
                         <li>
@@ -84,13 +93,13 @@ export class OrderScreen extends React.Component {
                         </select>
                         :
                         <div className="dp"><DateRange
-                        onChange={(select) => self.handleSelect(select, dateSelection)}
-                        ranges={[dateSelection]}
-                        maxDate={new Date()}
+                            onChange={(select) => self.handleSelect(select, dateSelection)}
+                            ranges={[dateSelection]}
+                            maxDate={new Date()}
                         /></div>
                     }
-                    
-                <div><button className="primary">Filter</button></div>
+
+                    <div><button className="primary">Filter</button></div>
                 </div> : null}
                 <div className="list">
                     {listMap}
