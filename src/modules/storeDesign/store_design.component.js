d53a89cc82b3f0d1c08560e3721568fa556ad936
sivaprakasharavi <sivaprakashravi@gmail.com>
2020-12-29 21:32:35 +0530

    store design ui updated

--- src/modules/storeDesign/store_design.component.js
+++ src/modules/storeDesign/store_design.component.js
@@ -1,42 +1,75 @@
 import React from 'react';
 import './store_design.style.scss';
-import {MdPhonelinkSetup,MdColorLens} from 'react-icons/md'
-class Storedesign extends React.Component{
-    constructor(props){
+import { MdPhonelinkSetup, MdColorLens } from 'react-icons/md'
+class Storedesign extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            services:[{
+        this.state = {
+            tabs: [{
                 label: 'Page',
                 route: '',
-                icon: <MdPhonelinkSetup size="35px" />
+                icon: <MdPhonelinkSetup size="24px" />,
+                view: this.pageDesign,
+                enabled: true
             },
             {
                 label: 'Theme',
                 route: '',
-                icon: <MdColorLens size="35px" />
+                icon: <MdColorLens size="24px" />,
+                view: this.themeDesign,
+                enabled: false
             }]
         }
     };
 
-    renderServices(){
-        return(
-            <ul>
-      {  this.state.services.map((x,index)=>{
-            return(
-                <li className="item-view" key={x.label}>
-                   {x.icon}
-                   <label>{x.label}</label>
-                </li>
-            )
-        })}
-        </ul>
+    tab() {
+        const tabs = this.state.tabs.length;
+        const selected = (tab) => {
+            const selectionUpdated = this.state.tabs.map(t => {
+                t.enabled = false;
+                if(t.label === tab.label) {
+                    t.enabled = true;
+                }
+                return t;
+            });
+            this.setState({tabs: selectionUpdated});
+        }
+        return (
+            <div className="tabs">
+                {  this.state.tabs.map((tab) => {
+                    return (
+                        <div className={tab.enabled ? 'active' : null} style={{ flex: `${100 / tabs}%` }} key={tab.label} onClick={() => selected(tab)}>
+                            {tab.icon}
+                            <label>{tab.label}</label>
+                        </div>
+                    )
+                })}
+            </div>
         )
     }
 
-    render(){
-        return(
+    pageDesign() {
+        return (
+            <div>page design</div>
+        )
+    }
+
+    themeDesign() {
+        return (
+            <div>theme design</div>
+        )
+    }
+
+    design() {
+        const tab =  this.state.tabs.find(t => t.enabled);
+        return tab.view();
+    }
+
+    render() {
+        return (
             <div className="store-design">
-               {this.renderServices()}
+                {this.tab()}
+                {this.design()}
             </div>
         )
     }
